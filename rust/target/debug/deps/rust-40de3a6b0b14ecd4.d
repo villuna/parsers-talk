71b51e3d47c1532301af05258778024ac48da044
Luna Borella <ulfrthewolf@gmail.com>
2023-10-10 16:37:38 +1000

    Remove rust idea

---
+++ rust/target/debug/deps/rust-40de3a6b0b14ecd4.d
@@ -0,0 +1,5 @@
+/home/luna/Documents/parsers/rust/target/debug/deps/rust-40de3a6b0b14ecd4.rmeta: src/lib.rs
+
+/home/luna/Documents/parsers/rust/target/debug/deps/rust-40de3a6b0b14ecd4.d: src/lib.rs
+
+src/lib.rs:
