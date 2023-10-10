e7eb338b920d2bbb4ecb4e6f1d072afc37df7c46
Luna Borella <ulfrthewolf@gmail.com>
2023-10-10 16:38:41 +1000

    Deleted some stuff that was still in the rust dir

--- rust/target/debug/deps/rust-010cf3ad37494e7f.d
+++
@@ -1,5 +0,0 @@
-/home/luna/Documents/parsers/rust/target/debug/deps/rust-010cf3ad37494e7f.rmeta: src/lib.rs
-
-/home/luna/Documents/parsers/rust/target/debug/deps/rust-010cf3ad37494e7f.d: src/lib.rs
-
-src/lib.rs:
