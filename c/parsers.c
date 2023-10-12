f72b6a0f70f643934ef195aa3d17a10ad7bfb1de
Luna Borella <ulfrthewolf@gmail.com>
2023-10-12 18:08:18 +1000

    Did some more c stuff, added many0

--- c/parsers.c
+++ c/parsers.c
@@ -103,22 +103,112 @@ parser_t two(parser_t one, parser_t two) {
     return result;
 }
 
+typedef struct {
+    parser_t parser;
+    size_t result_size;
+} many0_data;
+
+typedef struct {
+    void *list;
+    int length;
+} many0_result;
+
+char *many0_fn(char *input, void *vdata, void *vresult) {
+    many0_data *data = vdata;
+
+    // This is a strange thing
+    // we want to create a new array and write its memory address
+    // to the destination pointed at by vresult 
+    // if that makes any sense at all ._.
+    many0_result *result = vresult;
+
+    // If you dont know C, this is how your dynamic lists look like behind the scenes
+    void *out_list = NULL;
+    int length = 0;
+    int capacity = 0;
+
+    char *current_input = input;
+
+    // We don't know the size of our output at compile time,
+    // so we have to allocate it at runtime
+    void *output = malloc(data->result_size);
+
+    while (1) {
+        char *unparsed = data->parser.parse(current_input, data->parser.data, output);
+
+        if (unparsed == NULL) {
+            break;
+        }
+
+        current_input = unparsed;
+
+        // Push the new value to the vector
+        if (length >= capacity) {
+            // Resize the vector
+            if (capacity == 0) {
+                capacity = 1;
+            } else {
+                capacity *= 2;
+            }
+
+            out_list = realloc(out_list, data->result_size * capacity);
+        }
+
+        memcpy((void *) ((char *)out_list + length * data->result_size) , output, data->result_size);
+        length++;
+    }
+
+    out_list = realloc(out_list, data->result_size * length);
+    
+    free(output);
+
+    result->list = out_list;
+    result->length = length;
+
+    return current_input;
+}
+
+parser_t many0(parser_t parser, size_t result_size) {
+    many0_data *data = malloc(sizeof(many0_data));
+    data->parser = parser;
+    data->result_size = result_size;
+
+    parser_t result = {
+        .data = data,
+        .parse = many0_fn,
+    };
+
+    return result;
+}
+
 int main() {
-    char* my_string = "it works!";
-    slice_t r1;
+    char* my_string = "itititititworks!";
     char r2;
 
+    many0_result it_result;
+
     two_result result = {
-        .one = &r1,
+        .one = &it_result,
         .two = &r2,
     };
 
-    char* out = parse(two(tag("it"), character), my_string, &result);
+    char* out = parse(two(many0(tag("it"), sizeof(slice_t)), character), my_string, &result);
 
-    if (out) {
-        printf("output: (\"%.*s\", '%c', \"%s\")\n", r1.len, r1.string, r2, out);
-    } else {
-        printf("failed!\n");
+    if (out != NULL) {
+        printf("unconsumed output: \"%s\"\n", out);
+
+        printf("result: ([");
+
+        slice_t *slices = it_result.list;
+        for (int i = 0; i < it_result.length; i++) {
+            if (i != 0) {
+                printf(", ");
+            }
+
+            printf("\"%.*s\"", slices[i].len, slices[i].string);
+        }
+
+        printf("], ");
+        printf("'%c')\n", r2);
     }
-    
 }
